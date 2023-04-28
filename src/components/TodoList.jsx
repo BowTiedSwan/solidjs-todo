4e02d6c9fac67b3eeaa7530375e4e93fca893226
BowTiedSwan <0xf85ce443D3F031728e0e97503C06322c2F51A0c7@ethmail.cc>
2023-04-28 10:00:19 +0300

    add alert popup when task is due, add local storage

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,31 @@
-import { createSignal } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
+// import { GrAdd } from 'react-icons/gr';
 
 function TodoList() {
   const [todos, setTodos] = createSignal([]);
   const [newTodo, setNewTodo] = createSignal('');
   const [newDate, setNewDate] = createSignal('');
   const [newTime, setNewTime] = createSignal('');
+
+  onMount(() => {
+    console.log('onMount');
+
+    const intervalId = setInterval(() => {
+      const currentTime = new Date();
+      console.log('currentTime', currentTime);
+
+      todos().forEach((todo) => {
+        const todoTime = new Date(`${todo.date}T${todo.time}:00`);
+        console.log('todoTime', todoTime);
+
+        if (currentTime.getTime() === todoTime.getTime()) {
+          alert(`Todo: ${todo.text}`);
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  });
 
   function addTodo() {
     if (!newTodo() || !newDate() || !newTime()) {
@@ -40,6 +61,15 @@ function TodoList() {
     setNewTime(e.target.value);
   }
 
+  function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos()));
+  }
+
+  onMount(() => {
+    window.addEventListener('beforeunload', saveTodos);
+    return () => window.removeEventListener('beforeunload', saveTodos);
+  });
+
   return (
     <div class="bg-gray-900 min-h-screen text-white">
       <div class="max-w-md mx-auto p-4">
@@ -60,7 +90,10 @@ function TodoList() {
           <input type="text" value={newTodo()} onInput={handleNewTodoInput} placeholder="Enter a new todo" class="bg-gray-700 text-white py-2 px-4 rounded w-full" />
           <input type="date" value={newDate()} onInput={handleNewDateInput} class="bg-gray-700 text-white py-2 px-4 rounded" />
           <input type="time" value={newTime()} onInput={handleNewTimeInput} class="bg-gray-700 text-white py-2 px-4 rounded" />
-          <button class="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded" onClick={addTodo}>Add Todo</button>
+          <button class="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded text-white" onClick={addTodo}>
+            {/* <GrAdd /> */}
+            Add Todo
+          </button>
         </div>
       </div>
     </div>
